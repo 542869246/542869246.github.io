f363f21ac020914ebf9517a6ff0e9b000d1c4ae6
542869246 <542869246@qq.com>
2018-07-31 16:44:02 +0800

    Site updated: 2018-07-31 16:43:59

--- dist/music.js
+++ dist/music.js
@@ -3,6 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+	{
+        name: "Champagne Ocean",
+        artist: 'Ehrling',
+        url: 'https://music.163.com/outchain/player?type=2&id=436147067&auto=1&height=66&bg=e8e8e8',
+        cover: 'http://p1.music.126.net/3OCOqJx8ABWtsZejHlfTew==/18784056649624787.jpg?param=130y130',
+      },
       {
         name: "PDD洪荒之力",
         artist: '徐梦圆',
