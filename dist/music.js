8393b53f39c1dca0efdbcb3ee018887c38d22fbd
542869246 <542869246@qq.com>
2018-07-31 15:34:24 +0800

    Site updated: 2018-07-31 15:34:22

--- dist/music.js
+++ dist/music.js
@@ -3,7 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-      
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
       {
         name: '惊蛰',
         artist: '音阙诗听/王梓钰',
