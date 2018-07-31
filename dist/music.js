71411526e9e995883b6c7a5dbb428d85b1feb379
542869246 <542869246@qq.com>
2018-07-31 15:10:58 +0800

    Site updated: 2018-07-31 15:10:53

--- dist/music.js
+++ dist/music.js
@@ -3,35 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-      {
-        name: "Champagne Ocean",
-        artist: 'Ehrling',
-        url: 'http://m10.music.126.net/20180731144931/fdc877d8ecf4e5f528dc9fede4ec0f25/ymusic/4abf/29d0/d592/194366e4d3c6f37f860574f7c65d7c65.mp3',
-        cover: 'http://p1.music.126.net/3OCOqJx8ABWtsZejHlfTew==/18784056649624787.jpg?param=130y130',
-      },
-      {
-        name: 'River Flows in You',
-        artist: 'Yiruma',
-        url: 'http://m7.music.126.net/20180731144939/ca6b9bfb30deadf4b7fd2ed684d8f5e3/ymusic/UbaX7_8I4ZqjFjuELdsosQ==/509951162786707641',
-        cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
-      },
-      {
-        name: 'D大调卡农',
-        artist: '群星',
-        url: 'http://m8.music.126.net/20180731145144/3314232fb3d44783d91ca91f52d78943/ymusic/ibm10lIFdeQpMuLz_Rfjqg==/509951162816182841',
-        cover: 'http://p1.music.126.net/m3lZKXG9dGxyWY-WPgwZzA==/7972558813702741.jpg?param=130y130',
-      },
+      
       {
         name: '惊蛰',
         artist: '音阙诗听/王梓钰',
-        url: 'http://m10.music.126.net/20180731145747/7b0cad7ee69614f645f0b795f0041140/ymusic/9543/f8a5/52e2/897135fdcb0ef95c75e96df65f790994.mp3',
+        url: 'http://www.ytmp3.cn/down/48755.mp3',
         cover: 'http://p1.music.126.net/5MmXpaP9r88tNzExPGMI8Q==/109951163370350985.jpg?param=130y130',
-      },
-      {
-        name: 'Apologize',
-        artist: 'Martin Ermen',
-        url: 'http://m7.music.126.net/20180731145440/ad7113051f000333d445a1ad00ec0336/ymusic/MTIwODkxMTgwMQ==/b3370deb2203b18191e3fbb3af8bb729.mp3',
-        cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
       }
     ]
 });
