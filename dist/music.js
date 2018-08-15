32e8caa712cf570e1dc213bb886d0c802d3fa1dc
542869246 <542869246@qq.com>
2018-08-15 11:56:14 +0800

    Site updated: 2018-08-15 11:55:48

--- dist/music.js
+++ dist/music.js
@@ -6,19 +6,19 @@ const ap = new APlayer({
 	{
         name: "canon in d",
         artist: 'Brian Crain',
-        url: 'http://pd2tflnys.bkt.clouddn.com/Brian%20Crain%20-%20canon%20in%20d.mp3',
+        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Brian%20Crain%20-%20canon%20in%20d.mp3',
         cover: 'http://p1.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
       },
 	  {
         name: "Apologize",
         artist: 'Martin Ermen',
-        url: 'http://pd2tflnys.bkt.clouddn.com/Martin%20Ermen%20-%20Apologize.mp3',
+        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Martin%20Ermen%20-%20Apologize.mp3',
         cover: 'http://p1.music.126.net/-_6mcI4VV5IKaiwhUAytbg==/1791104441647901.jpg?param=130y130',
       },
 	  {
         name: "River Flows in You",
         artist: 'Yiruma',
-        url: 'http://pd2tflnys.bkt.clouddn.com/Yiruma%20-%20River%20Flows%20in%20You.flac',
+        url: 'https://yfzhou.oss-cn-beijing.aliyuncs.com/blog/music/Yiruma%20-%20River%20Flows%20in%20You.flac',
         cover: 'http://p1.music.126.net/8ZRSyI0ZN_4ah8uzsNd1mA==/2324367581169008.jpg?param=130y130',
       },
       {
